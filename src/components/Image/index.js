e0aa37fcbc2e0a09d17694d04604e8a54b2ac547
Julie Payen <julie.payen19@gmail.com>
2022-07-21 12:19:46 +0200

    Refactored

---
+++ src/components/Image/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import s from './index.module.css'
+
+export default function Image(props) {
+    return (
+        <img src={props.src} alt={props.alt} className={s.default} />
+    )
+}
