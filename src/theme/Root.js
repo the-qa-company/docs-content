7e1f9610a973c11d33feeab05b3a843561f5b42c
Clement Defretiere <defretiere.c@gmail.com>
2023-06-27 16:41:49 +0200

    Update widget

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -4,9 +4,9 @@ const iframeCode = `
 <iframe
   src='https://dev.qanswer.ai/widget?custom=%7B%22initialMsg%22%3A%22%22%7D&qaSystem=%7B%22name%22%3A%22qanswer-doc%22%2C%22owner%22%3A%22admin%22%2C%22type%22%3A%22text%22%7D'
   frameBorder='0'
-  style='border:none;overflow:hidden;position:fixed;right:16px;bottom:16px;z-index:999;'
+  style='border:none;overflow:hidden;display:none;position:fixed;right:16px;bottom:16px;z-index:9999;'
   allowtransparency='true'
-  onload='let iframe=this;window.addEventListener("message",function(e){if(e.data.type==="resize"){iframe.style.width=e.data.width;iframe.style.height=e.data.height;}});'
+  onload='let iframe=this;window.addEventListener("message",function(t){"resize"===t.data.type&&(iframe.style.width=t.data.width||iframe.style.width,iframe.style.height=t.data.height||iframe.style.height,iframe.style.right=t.data.right||iframe.style.right,iframe.style.bottom=t.data.bottom||iframe.style.bottom,iframe.style.display="block")});'
 />
 `
 
