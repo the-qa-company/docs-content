d2c5b6893458b14aa28d38722b3b8497e4edd73c
Dennis Diefenbach <D063520@users.noreply.github.com>
2023-09-30 14:42:19 +0200

    Fix widget

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -8,14 +8,10 @@ const iframeCode = `
     }
   }
 </style>
-<iframe
-  id='qanswer-widget'
-  src='https://app.qanswer.ai/widget?custom=%7B%22initialMsg%22%3A%22%22%7D&qaSystem=%7B%22name%22%3A%22QAnswer%20-%20Documentation%22%2C%22owner%22%3A%22demo%22%2C%22type%22%3A%22text%22%7D'
-  frameBorder='0'
-  style='border:none;overflow:hidden;display:none;position:fixed;right:16px;bottom:16px;z-index:9999;'
-  allowtransparency='true'
-  onload='let iframe=this;window.addEventListener("message",function(t){"resize"===t.data.type&&(iframe.style.width=t.data.width||iframe.style.width,iframe.style.height=t.data.height||iframe.style.height,iframe.style.right=t.data.right||iframe.style.right,iframe.style.bottom=t.data.bottom||iframe.style.bottom,iframe.style.display="block")});'
-/>
+<iframe 
+  src="https://app.qanswer.ai/widget?kb=QAnswer%20-%20Documentation&amp;user=demo&amp;type=text" 
+  frameborder="0" style="border:none;overflow:hidden;display:none;position:fixed;right:16px;bottom:16px;z-index:9999;" allowtransparency="true" onload="let iframe=this;window.addEventListener(&quot;message&quot;,function(t){&quot;resize&quot;===t.data.type&amp;&amp;(iframe.style.width=t.data.width||iframe.style.width,iframe.style.height=t.data.height||iframe.style.height,iframe.style.right=t.data.right||iframe.style.right,iframe.style.bottom=t.data.bottom||iframe.style.bottom,iframe.style.display=&quot;block&quot;)});">
+</iframe>
 `
 
 // Default implementation, that you can customize
