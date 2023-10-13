ba3860463ee252573395776df25347098b9623b8
J.M <jonathan.mallet3872@gmail.com>
2023-10-13 14:11:07 +0200

    Updating installation instruction

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,10 @@ const config = {
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
+            tableOfContents: {
+                minHeadingLevel: 2,
+                maxHeadingLevel: 5,
+              },
             navbar: {
                 title: 'Docs',
                 logo: {
