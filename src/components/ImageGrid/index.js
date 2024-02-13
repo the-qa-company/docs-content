2d06b028c4a28329c9fca669f78efcdb2d22847b
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2024-02-13 18:12:46 +0100

    Fixing problem with server side rendering failing

--- src/components/ImageGrid/index.js
+++ src/components/ImageGrid/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import s from './index.module.css'
 
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
 
 export default function ImageGrid({images, templateColumns = 'repeat(2, 1fr)', gap = '10px', noTransformation = false, columnHeight = undefined, gridMaxWidth = '100%'}) {
 
@@ -18,26 +20,32 @@ export default function ImageGrid({images, templateColumns = 'repeat(2, 1fr)', g
         }
     }
 
+    
     return (
-        <div className={s.grid} style={gridStyle}>
-            {images.map((image, index) => (
-                <div
-                    className={noTransformation  ? '' : s.imageWrapper}
-                    key={index}
-                >
-                    <img
-                        src={image.src}
-                        className={noTransformation ? s.basicImage : s.image}
-                        alt={image.alt || `Image ${index}`}
-                        onClick={() => handleClick(image.onClick)}
-                        style={{ cursor: image.onClick ? 'pointer' : 'default' }}
-                    />
-                    <div className={image.caption || image.description ? s.descContainer : ''}>
-                        {image.caption}
-                        {image.description}
+        <BrowserOnly>
+      {() =>
+            <div className={s.grid} style={gridStyle}>
+                {images.map((image, index) => (
+                    <div
+                        className={noTransformation  ? '' : s.imageWrapper}
+                        key={index}
+                    >
+                        <img
+                            src={image.src}
+                            className={noTransformation ? s.basicImage : s.image}
+                            alt={image.alt || `Image ${index}`}
+                            onClick={() => handleClick(image.onClick)}
+                            style={{ cursor: image.onClick ? 'pointer' : 'default' }}
+                        />
+                        <div className={image.caption || image.description ? s.descContainer : ''}>
+                            {image.caption}
+                            {image.description}
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        }
+        </BrowserOnly>
     )
+    
 }
